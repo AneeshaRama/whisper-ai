0bbdc6466140860d2de9b4d1d8958ed012aa073d
AneeshaRama <aneesharama@gmail.com>
2023-10-02 22:04:30 +0530

    Folder structure

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+
+const DashboardPage = () => {
+  return (
+    <h1>Dashboard (protected)</h1>
+  )
+}
+
+export default DashboardPage
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+
+
+
+const LandingPage = () => {
+  return (
+    <h1>Landing page(unprotected)</h1>
+  )
+}
+
+
+export default LandingPage
